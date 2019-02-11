73057d0a39d0b3c7f13797a255efdbef5f5d5850
admo <admoifgm@gmail.com>
2019-02-11 16:17:56 +0900

    3

---
+++ users/admo/3/index.ts
@@ -0,0 +1,32 @@
+import * as functions from 'firebase-functions'
+import * as admin from "firebase-admin";
+
+admin.initializeApp()
+admin.firestore().settings({ timestampsInSnapshots: true });
+
+export const onBostonWeatherUpdate = functions.firestore.document('citi-weather/bostom-ma-us').onUpdate(change => {
+	const after = change.after.data()
+	const payload = {
+		data: {
+			temp: String(after.temp),
+			conditions: after.conditions
+		}		
+	}
+	return admin.messaging().sendToTopic("weather_boston-ma-us", payload)
+});
+
+export const getBostonWeather = functions.https.onRequest((request, response) => {
+  //  const settings = {/* your settings... */ timestampsInSnapshots: true}
+    
+    admin.firestore().doc('citi-weather/bostom-ma-us').get()
+   //admin.firestore.settings(settings)
+    .then(snapshot => {
+        const data = snapshot.data()
+        response.send(data)
+    })
+    .catch(error => {
+        // Handle the error
+        console.log(error)
+        response.status(500).send(error)
+    })
+});
